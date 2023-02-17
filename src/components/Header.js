3a9c71625ba4e17f71efed82216e27350719c360
bastienlairis <bastien.lairis@gmail.com>
2023-02-17 11:14:17 +0100

    Add component Header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header
+      style={{
+        background: "salmon",
+        height: "100px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Je suis le header
+    </header>
+  );
+};
+
+export default Header;
