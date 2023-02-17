361589ffdce99502982d1e9cba34c61e31e580d5
bastienlairis <bastien.lairis@gmail.com>
2023-02-17 11:23:09 +0100

    update Header.js on master branch

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         alignItems: "center",
       }}
     >
-      Je suis le header
+      Je suis le header / Petit update de Header.js depuis la branche master
     </header>
   );
 };
