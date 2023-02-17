2026e7a29e62219786e2b0aeac0780ef7f272e98
Tom ROBERTO <tom.roberto31@gmail.com>
2023-02-17 11:49:05 +0100

    added component footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 
 //components
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h1>Hello github workshop !</h1>
       <p>Modification depuis vsCode</p>
       <p>Modification depuis Github par un autre dev</p>
+      <Footer />
     </div>
   );
 }
